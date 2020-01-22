9b3fb28902b5b28be36024319c5653c1ba08c767
norbikabai <norbikabai@gmail.com>
2020-01-22 14:01:17 +0100

    started redesigning project

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var css = document.querySelector("h3");
-var color1=document.querySelector(".color1");
-var color2=document.querySelector(".color2");
-var body=document.getElementById("gradient");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.querySelector("body");
 
-function setGradient()
-{
-    body.style.background="linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-    css.textContent=body.style.background+";";
+function setGradient() {
+    body.style.background =
+        "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+    css.textContent = body.style.background + ";";
 }
 
-color1.addEventListener("input", setGradient)
-color2.addEventListener("input", setGradient)
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
